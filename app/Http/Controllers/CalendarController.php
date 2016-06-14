847ec6b5a3dcdcccf7431698befe80767f8b9ef1
Shauna Gordon <shauna@shaunagordon.com>
2016-06-14 14:00:59 -0400

    Added default timezone set to handle timezone stuff in dates on the calendar.

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,6 +5,11 @@ use App\Providers\GoogleCalendarServiceProvider as Calendar;
 
 class CalendarController extends AppBaseController
 {
+	public function __construct()
+	{
+		date_default_timezone_set('America/New_York');
+	}
+
 	public function index() {
 		$calendar = new Calendar();
 		$events = $calendar->getEvents();
